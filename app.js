0535e5cde4063923262dd0909d6913f5e7494d38
Grzegorz Kędziora <geloke59@gmail.com>
2022-04-02 11:38:32 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send(`
+    <h1>Hello from this NodeJS app!</h1>
+    <p>HELLO KUBERNETES </p>
+    <p>Try sending a request to /error and see what happens</p>
+  `).status(200);
+});
+
+app.get('/error', (req, res) => {
+  console.log('Error ...');
+  process.exit(1);
+});
+setInterval(() => {
+  console.log('Serwer working ... on 8080');  
+}, 5000);
+app.listen(8080);
